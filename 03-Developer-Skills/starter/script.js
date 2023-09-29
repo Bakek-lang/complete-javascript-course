007a003fe23774b4f9aa2a6022bf1f9aa3deda3c
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2023-09-29 21:58:33 +0200

    updates

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,25 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// let arr = [17, 21, 23];
+
+// const printForecast = function (arr) {
+//   console.log(
+//     `... ${arr[0]} in 1 days ... ${arr[1]} in 2 days ... ${arr[2]} in 3 days ...`
+//   );
+// };
+
+// printForecast(arr);
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let str = "";
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]} in ${i + 1} days ...`;
+  }
+  console.log("... " + str);
+};
+
+printForecast(data1);
--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const checkButton = document.querySelector('.check');
+const message = document.querySelector('.message');
+const guessInput = document.querySelector('.guess');
+const score = document.querySelector('.score');
+const highscore = document.querySelector('.highscore');
+const againButton = document.querySelector('.again');
+const body = document.querySelector('body');
+const bigNumber = document.querySelector('.number');
+const randomNumber = Math.floor(Math.random() * 21);
+
+checkButton.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    message.textContent = '⛔️ No number!';
+  } else if (guess < 1) {
+    message.textContent = '❌ Number input too low';
+  } else if (guess > 20) {
+    message.textContent = '🚫 Number input too high';
+  } else {
+    let currentScore = Number(score.textContent);
+
+    if (guess > randomNumber) {
+      message.textContent = '📈 Too high!';
+      currentScore -= 1;
+      score.textContent = currentScore;
+    } else if (guess < randomNumber) {
+      message.textContent = '📉 Too low!';
+      currentScore -= 1;
+      score.textContent = currentScore;
+    } else if (guess === randomNumber) {
+      message.textContent = '🎉 Correct number!';
+      body.style.backgroundColor = 'green';
+      checkButton.style.display = 'none';
+      bigNumber.textContent = guess;
+    }
+  }
+});
+
+againButton.addEventListener('click', function () {
+  location.reload();
+  guessInput.value = '';
+});
